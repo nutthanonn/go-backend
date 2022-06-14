e17f95ec3aa81a6113afd870c74048500b46080c
nutthanon <nutthanon.tho@gmail.com>
2022-06-14 16:05:54 +0700

    update

--- README.md
+++
@@ -1,6 +0,0 @@
-# Golang Server
-
-<p align="center">
-<img src="./assets/go-logo.png" align="center" width="100" >
-
-</p>
--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Nutthanon! %s", time.Now())
+type todo struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{Id: "1", Title: "nut", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func addTodos(context *gin.Context) {
+	var newTodos todo
+
+	if err := context.BindJSON(&newTodos); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodos)
+	context.IndentedJSON(http.StatusCreated, newTodos)
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodos)
+	router.Run("localhost:8000")
 }
