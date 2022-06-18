17b194b29a775ff50867d8d39875653e0e4500df
nutthanon <nutthanon.tho@gmail.com>
2022-06-19 02:51:12 +0700

    [+] add: add gorm

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nutthanonn/golang-webserver/pkg/db"
+	"github.com/nutthanonn/golang-webserver/pkg/handlers"
+)
+
+func main() {
+	DB := db.Init()
+	h := handlers.New(DB)
+	router := gin.Default()
+
+	router.GET("year", h.GetYear)
+	router.Run("localhost:8000")
+
+}
