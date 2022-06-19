37e7f5c1ecd172c9ce48faa871e91e9766d5db27
nutthanon <nutthanon.tho@gmail.com>
2022-06-19 20:36:36 +0700

    [+] update: change gorm type

--- pkg/models/year.go
+++ pkg/models/year.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Year struct {
-	Year_id     string    `json:"year_id" gorm:"primary_key"`
+	Year_id     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
 	Year_name   string    `json:"year_name"`
 	Create_date time.Time `json:"create_date"`
 	Update_date time.Time `json:"update_date"`
