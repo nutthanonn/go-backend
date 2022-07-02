f196a8ed4b4b426ff8672e3f812224766e2bf904
nutthanon <nutthanon.tho@gmail.com>
2022-07-03 01:15:39 +0700

    [+] update: unique

--- pkg/models/product_types.go
+++ pkg/models/product_types.go
@@ -9,7 +9,7 @@ import (
 type Product_types struct {
 	Product_type_id   uuid.UUID  `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	Product_type_name string     `json:"product_type_name"`
-	Products          []Products `gorm:"foreignKey:Product_type_id"`
+	Products          []Products `gorm:"foreignKey:Product_type_id;not null;unique"`
 	Create_at         time.Time
 	Update_at         time.Time
 	Delete_at         *time.Time
